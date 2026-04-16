468bba5310c0ebfabdfdb2a285819549c7d056b6
terror-akshat <www.akshatagarwal123klj@gmail.com>
2026-04-16 17:43:40 +0530

    adding backend spring boot | and frontend using react

---
+++ Backend/src/test/java/nodemons/example/com/ComApplicationTests.java
@@ -0,0 +1,13 @@
+package nodemons.example.com;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ComApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
