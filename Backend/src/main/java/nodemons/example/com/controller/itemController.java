5f29ff2d74c710f90f3ff3a981fb217372a4bd5d
terror-akshat <www.akshatagarwal123klj@gmail.com>
2026-04-17 00:03:58 +0530

    fixes

--- Backend/src/main/java/nodemons/example/com/controller/itemController.java
+++ Backend/src/main/java/nodemons/example/com/controller/itemController.java
@@ -8,10 +8,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.Collections;
 import java.util.List;
-
-import static org.springframework.http.ResponseEntity.status;
-
 
 @RestController
 @RequestMapping("/items")
@@ -33,10 +31,11 @@ public class itemController {
     }
 
     @GetMapping
-    public ResponseEntity<Item> getAllItems(@RequestBody Item item) {
+    public ResponseEntity<List<Item> getAllItems(@RequestBody Item item) {
         List<Item> items = itemService.getAllItem();
-        return ResponseEntity.status(HttpStatus.FOUND).body((Item) items);
+        return ResponseEntity.status(HttpStatus.FOUND).body(Collections.singletonList((Item) items));
     }
+
     @GetMapping("/claim-item")
     public ResponseEntity<Item> claim(@RequestBody int id) {
         Item item = itemService.claimItem(id);
