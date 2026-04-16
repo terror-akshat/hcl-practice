730a8838aa1883347f7a84ea21f9053d7fafda06
MUS-AB-7 <2k22.cse.2213268@gmail.com>
2026-04-16 23:18:11 +0530

    Added getter setters and generation type

--- Backend/src/main/java/nodemons/example/com/Entity/Item.java
+++ Backend/src/main/java/nodemons/example/com/Entity/Item.java
@@ -3,11 +3,17 @@ package nodemons.example.com.Entity;
 import java.time.LocalDateTime;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import lombok.Data;
 
+@Data
 @Entity
 public class Item {
+
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int item_id;
     private String item_name;
     private String item_description;
